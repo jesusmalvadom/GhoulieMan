03e16fc0f3afa0c4773f6513ffb8445b106ee33b
Jesús Miguel Álvarez <suso1996@hotmail.com>
2019-02-28 17:52:51 +0100

    Initial Commit
    Chapter 2. Page 59

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float maxSpeed = 6.0f;
+    public float moveDirection;
+    public bool facingRight = true;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody> ();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = Input.GetAxis("Horizontal");
+    }
+
+	void FixedUpdate()
+	{
+        rb.velocity = new Vector2(moveDirection * maxSpeed, rb.velocity.y);
+	}
+}
