86742aaf69e8771c4bf0bc6fe79ccd43a349b886
Jesús Miguel Álvarez <suso1996@hotmail.com>
2019-03-04 18:35:26 +0100

    Pagina 105
    Hay que depurar la animación de salto del personaje principal

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -8,22 +8,50 @@ public class CharacterMovement : MonoBehaviour
     public float moveDirection;
     public bool facingRight = true;
     private Rigidbody rb;
+    private Animator anim;
+
+    public float jumpSpeed = 100.0f;
+
+    public bool grounded = false;
+    public Transform groundCheck;
+    public float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody> ();
-        
+        anim = GetComponent<Animator>();
+        groundCheck = GameObject.Find("GroundCheck").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
         moveDirection = Input.GetAxis("Horizontal");
+
+        if (grounded && Input.GetButtonDown("Jump")) {
+            anim.SetTrigger("isJumping");
+            rb.AddForce(new Vector2(0, jumpSpeed));
+        }
     }
 
 	void FixedUpdate()
 	{
         rb.velocity = new Vector2(moveDirection * maxSpeed, rb.velocity.y);
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+
+        if (moveDirection > 0.0f && !facingRight){
+            Flip();
+        } else if (moveDirection < 0.0f && facingRight) {
+            Flip();
+        }
+
+        anim.SetFloat("Speed", Mathf.Abs(moveDirection));
 	}
+
+    void Flip () {
+        facingRight = !facingRight;
+        transform.Rotate(Vector3.up, 180.0f, Space.World);
+    }
 }
